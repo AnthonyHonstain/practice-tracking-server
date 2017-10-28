7816ba3c97b4696a46f4cff313d516e132963eff
Anthony Honstain <honstain@gmail.com>
2017-10-28 15:17:01 -0700

    Attempting to pull in a dynamic port from heroku for prod

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import Server from 'socket.io';
 
 export function startServer(store) {
-  const io = new Server().attach(8090);
+  const io = new Server().attach(process.env.PORT || 8090);
 
   store.subscribe(() => {
     console.log('subscribe', store.getState());
